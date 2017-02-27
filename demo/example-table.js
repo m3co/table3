a2d4cdea0344c3c132d0b58eb7ce92ba179a57ae
Andres Rios <rianby64@gmail.com>
2017-02-27 21:21:20 +0300

    Start from here

---
+++ demo/example-table.js
@@ -0,0 +1,15 @@
+'use strict';
+var data = function () {
+  return Handsontable.helper.createSpreadsheetData(100, 10);
+};
+
+var container = document.getElementById('example');
+
+var hot = new Handsontable(container, {
+  data: data(),
+  minSpareCols: 1,
+  minSpareRows: 1,
+  rowHeaders: true,
+  colHeaders: true,
+  contextMenu: true
+});
