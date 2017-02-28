7136ba778b7056c574ad2d6a6c593d248bf1be41
Andres Rios <rianby64@gmail.com>
2017-02-28 15:37:04 +0300

    [DEMO] Show that D3 may be the path to take...

--- demo/example-table.js
+++ demo/example-table.js
@@ -1,15 +1,32 @@
 'use strict';
-var data = function () {
-  return Handsontable.helper.createSpreadsheetData(100, 10);
-};
 
-var container = document.getElementById('example');
+var data = [
+  {id:1, name:"Oli Bob \n steve", age:"12", gender:"male", height:1, col:"red", dob:"", cheese:1, lucky_no:5},
+  {id:2, name:"Mary May", age:"1", gender:"female", height:2, col:"blue", dob:"14/05/1982", cheese:true, lucky_no:10},
+  {id:3, name:"Christine Lobowski", age:"42", height:0, col:"green", dob:"22/05/1982", cheese:"true", lucky_no:12},
+  {id:4, name:"Brendon Philips", age:"125", gender:"male", height:1, col:"orange", dob:"01/08/1980", lucky_no:18},
+  {id:5, name:"Margret Marmajuke", age:"16", gender:"female", height:5, col:"yellow", dob:"31/01/1999", lucky_no:33},
+  {id:6, name:"Frank Harbours", age:"38", gender:"male", height:4, col:"red", dob:"", cheese:1, lucky_no:2},
+  {id:7, name:"Jamie Newhart", age:"23", gender:"male", height:3, col:"green", dob:"14/05/1985", cheese:true, lucky_no:63},
+  {id:8, name:"Gemma Jane", age:"60", height:0, col:"red", dob:"22/05/1982", cheese:"true", lucky_no:72},
+  {id:9, name:"Emily Sykes", age:"42", gender:"female", height:1, col:"maroon", dob:"11/11/1970", lucky_no:44},
+  {id:10, name:"James Newman", age:"73", gender:"male", height:5, col:"red", dob:"22/03/1998", lucky_no:9},
+];
 
-var hot = new Handsontable(container, {
-  data: data(),
-  minSpareCols: 1,
-  minSpareRows: 1,
-  rowHeaders: true,
-  colHeaders: true,
-  contextMenu: true
-});
+var table = d3.select('#example-table');
+var columns = Object.keys(data[0] ? data[0] : {});
+
+table.select('thead tr').selectAll('th')
+  .data(columns)
+  .enter()
+  .append('th')
+  .text(d => d);
+
+table.select('tbody').selectAll('tr')
+  .data(data)
+  .enter()
+  .append('tr').selectAll('td')
+  .data(d => columns.map(c => d[c]))
+  .enter()
+  .append('td')
+  .text(d => d);
