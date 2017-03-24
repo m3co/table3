07ff91c4af96213144d4accda7da62b6da2c1c51
Andres Rios <rianby64@gmail.com>
2017-03-24 15:03:45 +0300

    Setup columns in a very primitive way

--- table-ce.js
+++ table-ce.js
@@ -4,10 +4,33 @@
   class HTMLTableElement extends HTMLElement {
     constructor() {
       super();
+
+      // It's not in D3 flavour...
+      var table = document.createElement('table');
+      var thead = table.querySelector('thead');
+      if (!thead) {
+        thead = table.appendChild(document.createElement('thead'));
+      }
+
+      var columns_ = [];
+      Object.defineProperty(this, 'columns', {
+        get: () => columns_,
+        set: (columns) => {
+          thead.innerHTML = '<tr></tr>';
+          var tr = thead.querySelector('tr');
+          columns_ = [...columns].map(d => {
+            var th = document.createElement('th');
+            th.textContent = d;
+            tr.appendChild(th);
+            return d;
+          });
+        }
+      });
+      this._table = table;
     }
 
-    setup() {
-
+    connectedCallback() {
+      this.appendChild(this._table);
     }
 
   }
--- test/index.js
+++ test/index.js
@@ -3,5 +3,40 @@ test(() => {
 
   var table3 = document.createElement('x-table');
 
-  assert_true(table3.setup instanceof Function, "setup fn is present");
+  assert_true(table3.columns instanceof Array, "columns is present");
 }, "Table3 element has a defined API");
+
+test(() => {
+  // [SETUP]
+  var table3 = document.createElement('x-table');
+
+  // First variant that defines the "columns"
+  table3.columns = ['Title 1', 'Title 2', 'Title 3'];
+
+  // Second variant that defines the "columns"
+  /*
+  table3.columns = [
+    { title: "Title 1" },
+    { title: "Title 2" },
+    { title: "Title 3" }
+  ];
+  */
+
+  // Both cases should be considered... but let's implement at least one of them
+  // First case is the case to implement...
+  // [RUN]
+  document.body.appendChild(table3);
+
+  // [VERIFY]
+  [...table3.querySelectorAll('th')].forEach((th, i) => {
+    assert_equals(th.textContent, table3.columns[i]);
+  });
+
+  table3.columns = ['New title 1', 'New title 2'];
+  [...table3.querySelectorAll('th')].forEach((th, i) => {
+    assert_equals(th.textContent, table3.columns[i]);
+  });
+
+  // [TEARDOWN]
+  document.body.removeChild(table3);
+}, "Setup columns");
