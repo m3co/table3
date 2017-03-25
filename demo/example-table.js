ccb8889aa1ec6984c2a9065ccc3eff937366d55a
Andres Rios <rianby64@gmail.com>
2017-03-25 16:23:58 +0300

    [DEMO] Rewrite filter

--- demo/example-table.js
+++ demo/example-table.js
@@ -1,4 +1,8 @@
 'use strict';
 document.currentFragment.loaded.then(fragment => {
-
+  var filter = fragment.querySelector('#filter');
+  var table = fragment.querySelector('x-table');
+  filter.addEventListener('keyup', e => {
+    table.filter = e.target.value;
+  });
 });
