3e2f2463f751ace0be9c4214f45ded9f02a49045
Andres Rios <rianby64@gmail.com>
2017-04-10 11:19:44 +0300

    [DEMO] Fix the way to load data

--- trunk/fetcher.js
+++ trunk/fetcher.js
@@ -4,12 +4,14 @@ document.currentFragment.loaded.then(fragment => {
   fetch(url)
     .then(response => response.json())
     .then(json => {
-      var table3 = document.querySelector('x-table');
-      table3.data = json.data.map(item => {
-        return Object.keys(item.attributes).map(function(key) {
-          return this[key];
-        }, item.attributes);
+      document.querySelector('[src="/trunk/example-table.html"]').loaded.then((fragment) => {
+        var table3 = fragment.querySelector('x-table');
+        table3.data = json.data.map(item => {
+          return Object.keys(item.attributes).map(function(key) {
+            return this[key];
+          }, item.attributes);
+        });
+        table3.columns = Object.keys(json.data[0].attributes);
       });
-      table3.columns = Object.keys(json.data[0].attributes);
     });
 });
